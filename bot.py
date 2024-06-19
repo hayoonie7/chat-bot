a874548eaee79c73f98f7278f1bbc3124e26cba1
mike-935 <Michael935935@gmail.com>
2024-06-19 01:49:21 -0400

    Started to setup using a database to store user info

--- bot.py
+++ bot.py
@@ -3,16 +3,25 @@ from discord import app_commands
 import responses
 import os
 import league
+import mysql.connector
+
 from dotenv import load_dotenv, dotenv_values
 
 load_dotenv()
 token = os.getenv('token')
 server_id = os.getenv('server_id')
+user = os.getenv('user')
+host = os.getenv('host')
+database = os.getenv('database')
+password = os.getenv('password')
 print(f'Token: {token}')
 
 class GitGudBot(discord.Client):
     def __init__(self):
-        super().__init__(intents=discord.Intents.default())
+        intents = discord.Intents.default()
+        intents.guilds = True  # Ensure the guilds intent is enabled
+        intents.messages = True  # Ensure the messages intent is enabled
+        super().__init__(intents=intents)
         self.synced = False
         
     async def send_message(self, message, user_message):
@@ -39,18 +48,43 @@ class GitGudBot(discord.Client):
     async def on_ready(self):
         print(f'Bot {self.user} is ready!')
         await self.wait_until_ready()
+        await self.connect_database()
         if not self.synced:
             await command_tree.sync(guild = discord.Object(id=server_id))
             self.synced = True
+    
+    async def on_guild_join(self, guild):
+        print(f'Joined {guild.name}!')
 
     async def on_message(self, message):
-        if message.author == self.user or message[0] == '/':
+        if message.author == self.user:
             return
         user = str(message.author)
         user_message = str(message.content)
+        if user_message.startswith('/'):
+            return
         channel = str(message.channel)
         print(f'{user} in {channel} sent: {user_message}')
         await self.send_message(message, user_message)
+
+    
+    async def connect_database(self):
+        try:
+            cnx = mysql.connector.connect(user=user, password=password,
+                              host=host,
+                              database=database, port=3306)
+            if cnx.is_connected():
+                db_Info = cnx.get_server_info()
+                print("Connected to MySQL Server version ", db_Info)
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                print("Something is wrong with your user name or password")
+            elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                print("Database does not exist")
+            else:
+                print(err)
+            
+        cnx.close()
 
 
 
